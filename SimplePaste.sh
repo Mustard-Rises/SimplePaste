fb28ad087b121b7ce6a541972af2c4c093914a74
Mustard-Rises <108833878+Mustard-Rises@users.noreply.github.com>
2022-12-13 17:31:03 +0200

    Update SimplePaste.sh

--- SimplePaste.sh
+++ SimplePaste.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-xarg='-x'
-carg='-c'
-varg='-v'
+xarg='x'
+carg='c'
+varg='v'
 count=1
 
-Temp_Dir=~/.config/CopyUtils/tmp/Dir
-Temp_File=~/.config/CopyUtils/tmp/File
-touch $Temp_Dir
-touch $Temp_File
+tmp=~/.config/SimplePaste/tmp
+Temp_Dir=$tmp/Dir
+Temp_File=$tmp/File
+Pwd=$(pwd)/
 
 if [ "$#" -lt 2 ]; then
 	count=0
@@ -31,8 +31,27 @@ elif [[ "$carg" == "$1" && "$count" -eq 1 ]]; then
 	done
 
 elif [[ "$varg" == "$1" ]]; then
-
-
+	Dir=`cat $Temp_Dir`
+	read line < "$Temp_File"
+	
+	if [[ "$line" == "$xarg" ]]; then
+		{
+		read
+		while read line; 
+		do
+			mv  "$Dir"/"$line" "$Pwd"
+		done
+		} < "$Temp_File"
+	else 
+		{
+		read
+		while read line; 
+		do
+			cp  "$Dir"/"$line" "$Pwd"
+		done
+		} < "$Temp_File"
+	fi
+	
 else echo "script usage: $(basename $0) [-x name] [-c name] [-v]"
 exit 1
 fi
