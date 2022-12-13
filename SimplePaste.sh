6b4d4ff308f1183c1e80bd8c145d505f970df5da
Mustard-Rises <108833878+Mustard-Rises@users.noreply.github.com>
2022-12-13 15:16:16 +0200

    Add files via upload

---
+++ SimplePaste.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+xarg='-x'
+carg='-c'
+varg='-v'
+count=1
+
+Temp_Dir=~/.config/CopyUtils/tmp/Dir
+Temp_File=~/.config/CopyUtils/tmp/File
+touch $Temp_Dir
+touch $Temp_File
+
+if [ "$#" -lt 2 ]; then
+	count=0
+fi
+
+if [[ "$xarg" == "$1" && "$count" -eq 1 ]]; then
+	echo $(pwd) > $Temp_Dir
+	echo "x" > $Temp_File
+	for var in "${@:2}"
+	do 	
+		echo "$var" >> $Temp_File
+	done
+	echo "x"
+
+elif [[ "$carg" == "$1" && "$count" -eq 1 ]]; then
+	echo $(pwd) > $Temp_Dir
+	echo "c" > $Temp_File
+	for var in "${@:2}"
+	do 	
+		echo "$var" >> $Temp_File
+	done
+	echo "c"
+
+elif [[ "$varg" == "$1" ]]; then
+	echo "v"
+
+else echo "script usage: $(basename $0) [-x name] [-c name] [-v]"
+exit 1
+fi
